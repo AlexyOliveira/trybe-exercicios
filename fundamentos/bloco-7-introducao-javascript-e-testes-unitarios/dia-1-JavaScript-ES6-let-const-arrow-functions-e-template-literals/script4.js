eba4cdaf5fb8af3bdc5df035b437f7162b4534de
Alexandre Oliveira <cupofteaoficial@gmail.com>
2022-09-18 11:48:59 -0300

    all done

--- fundamentos/bloco-7-introducao-javascript-e-testes-unitarios/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script4.js
+++ fundamentos/bloco-7-introducao-javascript-e-testes-unitarios/dia-1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/script4.js
@@ -1,16 +1,17 @@
-let maiorPalavra = (frase) =>{
-  let array = frase.split(" ")
-  let wordLength = 0
-  let result = ''
+let maiorPalavra = (frase) => {
+  let array = frase.split(" ");
+  let wordLength = 0;
+  let result = "";
 
   for (const frases of array) {
     if (frases.length > wordLength) {
-        wordLength = frases.length
-        result = frases
-        
+      wordLength = frases.length;
+      result = frases;
     }
   }
-  return result
-}
+  return result;
+};
 
-console.log(maiorPalavra('Antônio foi ao banheiro e não sabemos o que aconteceu'))
+console.log(
+  maiorPalavra("Antônio foi ao banheiro e não sabemos o que aconteceu")
+);
