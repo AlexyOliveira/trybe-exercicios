949449fe8db1a3b1fc0073d458fc4717a5e0d848
Alexandre Oliveira <cupofteaoficial@gmail.com>
2022-09-19 18:43:07 -0300

    Exercício 1:Teste se o retorno de sum(0, 0) é 0(ok)

--- fundamentos/bloco-7-introducao-javascript-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-javascript-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -4,4 +4,7 @@ describe("Exercício 1, sum", () => {
   it("retorno de sum(4, 5) é 9", () => {
     expect(sum(4, 5)).toBe(9)
   });
+  it('retorno de sum(0, 0) é 0',()=>{
+    expect(sum(0, 0)).toBe(0)
+  });
 });
