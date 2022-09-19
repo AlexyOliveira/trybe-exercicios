6200801695dc473b42c4d735e14eae807120932e
Alexandre Oliveira <cupofteaoficial@gmail.com>
2022-09-19 18:48:59 -0300

    Exercício 1: Teste se a função sum lança um erro quando os parâmetros são 4 e "5"(string 5)(ok)

--- fundamentos/bloco-7-introducao-javascript-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-javascript-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -2,9 +2,12 @@ const sum = require("./sum");
 
 describe("Exercício 1, sum", () => {
   it("retorno de sum(4, 5) é 9", () => {
-    expect(sum(4, 5)).toBe(9)
+    expect(sum(4, 5)).toBe(9);
   });
-  it('retorno de sum(0, 0) é 0',()=>{
-    expect(sum(0, 0)).toBe(0)
+  it("retorno de sum(0, 0) é 0", () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+  it('lança um erro quando os parâmetros são 4 e "5"(string 5)', () => {
+    expect(() => compileAndroidCode()).toThrow();
   });
 });
