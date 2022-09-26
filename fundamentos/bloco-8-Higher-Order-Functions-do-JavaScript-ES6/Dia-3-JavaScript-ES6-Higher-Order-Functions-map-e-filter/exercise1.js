6673b242a9d06817f87167dd807b4eee15d06bbe
Alexandre Oliveira <cupofteaoficial@gmail.com>
2022-09-26 19:17:38 -0300

    Exercise 2 done

--- fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/Dia-3-JavaScript-ES6-Higher-Order-Functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/Dia-3-JavaScript-ES6-Higher-Order-Functions-map-e-filter/exercise1.js
@@ -63,14 +63,14 @@ const books = [
   
   // Adicione o código do exercício aqui:
 
-  const expectedResult = [
-    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-    'Fundação - Ficção Científica - Isaac Asimov',
-    'Duna - Ficção Científica - Frank Herbert',
-    'A Coisa - Terror - Stephen King',
-    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
-  ];
+//   const expectedResult = [
+//     'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+//     'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+//     'Fundação - Ficção Científica - Isaac Asimov',
+//     'Duna - Ficção Científica - Frank Herbert',
+//     'A Coisa - Terror - Stephen King',
+//     'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+//   ];
   
   function formatedBookNames(books) {
     const livros =  books.map((book)=> `${book.name} - ${book.genre} - ${book.author.name}`);
